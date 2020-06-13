cd3add1c63dbf9a7d55ad12c936e565ef44f464a
Ryan Wholey <ryan@takescoop.com>
2020-06-12 17:19:35 -0700

    Add namepsace output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "name" {
   value       = kubernetes_secret.docker.metadata[0].name
 }
 
+output "namespace" {
+  description = "The namespace of the secret"
+  value       = kubernetes_secret.docker.metadata[0].namespace
+}
+
 output "data" {
   description = "The secret data"
   value       = kubernetes_secret.docker.data
